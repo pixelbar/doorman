a44ebeffab006f7cd9f78a3fb8776e8964f676d5
David <david@awkward.co>
2019-03-14 15:09:49 +0100

    Clean up

--- server/index.js
+++ server/index.js
@@ -11,15 +11,14 @@ const userData = [{ name: 'Tim', publicKey: '3323b07a05000075', privateKey: '395
 
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
-const port = new SerialPort('/dev/tty.wchusbserial1420', { baudRate: 115200 })
+const port = new SerialPort('/dev/cu.wchusbserial1440', { baudRate: 115200 })
 
 let firstCheck = false
 let secondCheck = false
 
-var challenge
-var page
+let challenge, page, element
 
-const parser = new Readline()
+const parser = new Readline({ delimiter: '\r\n' })
 port.pipe(parser)
 
 setInterval(() => {
@@ -31,81 +30,88 @@ port.write('K\n')
 
 parser.on('open', () => console.log('Comm is open!'))
 
+const reset = () => {
+  // setTimeout(() => {
+    firstCheck = false
+    secondCheck = false
+    element = null
+    challenge = null
+  // }, 1000)
+}
+
 const fn = async () => {
   //await client.connect();
   parser.on('data', line => {
     const results = /<(.*)>/g.exec(line)
-
-    if(!results || !results.length) {
-      return
-    }
-
+    if(!results) return
     const result = results[1]
 
     //CHECK IF IS IBUTTON  
     if (result.length === 16) {
       //client.query('SELECT * FROM Users WHERE public_key = $1;', [result])
       // .then((response) => {
-      const element = userData.find(el => { return el.publicKey.toLowerCase() === result.toLowerCase() })
+      element = userData.find(el => { return el.publicKey.toLowerCase() === result.toLowerCase() })
       if (!element) {
         // no results
+        reset()
       } else {
         if(firstCheck) {
+          reset()
           return
         }
         firstCheck = true
-        console.log("found match", element)
-        // const hash = crypto.createHash('sha1').update(`C^AADC`).digest('HEX')
+        // const hash = crypto.createHash('sha1').update(`C^AADC`).digest('H/EX')
+
         page = String.fromCharCode(Math.round(Math.random() * (4 - 0) + 0))
-        challenge = Math.random().toString(36).substr(2, 4)
+        challenge = Math.random().toString(36).substr(2, 3)
         
         port.write(`C${page}${challenge}\n`)
-        //      verify.update(element.privateKey)
-        //      if(!verify.verify(result, signature)) {
-        // not a valid key
-        //        port.write('N\n')
-        //      } else {
-        //        port.write('A\n')
-        //      }
       }
-      //  }).catch(console.log)
     } else if (result.length === 105) {
       if(secondCheck) {
+        reset()
         return
       }
       let object = /([0-9A-Fa-f]{64}) ([0-9A-Fa-f]{40})/i.exec(result)
       if (object === undefined || object[1] === undefined || object[2] === undefined) {
-    	// If no object could be parsed, we deny access
+    	  // If no object could be parsed, we deny access
       	port.write("N\n")
       	return
       }
-      let data = object[1]
-      let mac = object[2]
-      console.log('going to open')
+      
+      const data = object[1]
+      const mac = object[2]
+      
       secondCheck = true
-      if (challenge === undefined) {
-      // If no challenge was set, we deny access
-      	port.write("N\n")
+      if (!challenge) {
+        // If no challenge was set, we deny access
+        port.write("N\n")
+        reset()
       	return
       }
+
+      console.log(data, mac)
+      
+      // my $wanted = read_mac($id, $secret, $page, $data, $challenge);
+
       var wanted = "" // Here we need to calculate something!
+
+      // const verify = crypto.createVerify('sha1')
+      // verify.update(element.privateKey)
+      // verify.end()
+      // console.log('YYAAAAAY', verify.verify(element.publicKey, result))
       
       if (mac.toUpperCase() !== wanted.toUpperCase()) {
         // If they do not match, we deny access
-      	port.write("N\n")
+        console.log('no match')
+        port.write("N\n")
+        reset()
       	return
       }
-      
-      
-      
-      // Remove the challenge
-      delete challenge
-      //const challenge = result.split(' ')[1]
-      // should do second challenge here!
+
+      // second extended challenge?
       port.write("A\n")
-      setInterval(() => {
-        firstCheck = secondCheck = false
-      }, 2500)
+      reset()
     }
   })
 }
